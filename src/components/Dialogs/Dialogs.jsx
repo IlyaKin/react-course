fb90af534f113a60b7ee0431110f734d5565abd5
IlyaKin <kin.1991@inbox.ru>
2022-06-05 22:34:27 +0300

    dialogs page html

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -2,8 +2,35 @@ import cl from './Dialogs.module.css'
 
 const Dialogs = (props)=>{
     return(
-        <div>
-            Dialogs
+        <div className={cl.dialogs}>
+            <div className={cl.dialogsItems}>
+                <div className={cl.dialog}>
+                    Alexey
+                </div>
+                <div className={cl.dialog}>
+                    Irina
+                </div>
+                <div className={cl.dialog}>
+                    Sergey
+                </div>
+                <div className={cl.dialog}>
+                    Ivan
+                </div>
+                <div className={cl.dialog}>
+                    Alena
+                </div>
+            </div>
+            <div className={cl.messages}>
+                <div className={cl.message}>
+                    Whats up
+                </div>
+                <div className={cl.message}>
+                    Who are you?
+                </div>
+                <div className={cl.message}>
+                    Shut up
+                </div>
+            </div>
         </div>
     )
 }
