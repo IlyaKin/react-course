9fce6f9a416ebdf21b6a34c01ab2083180fc74fe
unknown <Ilya_Kin@MSTB-PDTR-02.corp.mashtab.org>
2022-06-08 17:18:03 +0300

    adding posts to state

--- src/components/Content/MyPosts/MyPosts.jsx
+++ src/components/Content/MyPosts/MyPosts.jsx
@@ -1,19 +1,24 @@
 import cl from './MyPosts.module.css'
 import Post from "./Posts/Post";
+import React from 'react';
 
 const MyPosts = (props) => {
-
+    let newPost = React.createRef();
+    let addPost = ()=>{
+        let text = newPost.current.value;
+        alert(text);
+    }
 
     let posts = props.posts.map(p => <Post likes={p.likes} text={p.text}/>)
     return (
         <div className={cl.postsBlock}>
             <h3>My Posts</h3>
-            <div><textarea>
-
+            <div>
+                <textarea ref={newPost}>
             </textarea>
             </div>
             <div>
-                <button>Add</button>
+                <button onClick={addPost}>Add</button>
             </div>
             <div className={cl.posts}>
                 {posts}
